f9ae9ed4c0cbee43fea5a56455a72dfac43af937
gonzalogacc <gonzalogacc@gmail.com>
2014-10-10 13:56:55 +0100

    css

--- controllers/assemblies.py
+++ controllers/assemblies.py
@@ -11,12 +11,17 @@ def assembly():
     assembled_seq = db(db.assembled_seq.id == id).select()[0]
 
     specie = db((db.assembly_run.id == assembled_seq.assembly_run) & (db.specie.id == db.assembly_run.specie)).select(db.specie.ALL)[0]
-    print specie
 
     ## un dataset produce solo un ensamblado
     dataset = db((db.assembly_run.id == assembled_seq.assembly_run) & (db.dataset.specie == specie.id)).select(db.dataset.ALL)[0]
 
     ## an assembly run yields only one assembled_seq
     assembly_run = db(db.assembly_run.id == assembled_seq.assembly_run).select(db.assembly_run.ALL)[0]
+
+    print "deaca"
+    print assembled_seq
+    print specie
+    print dataset
+    print assembly_run
 
     return {'assembled_seq': assembled_seq, 'specie': specie, 'dataset': dataset, 'assembly_run': assembly_run}
